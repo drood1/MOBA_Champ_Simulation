fa5b663a35295bacf92160d93edc96ad9bdb636d
Karl Olsen <droodicus@gmail.com>
2017-07-03 17:31:50 -0400

    Made turrets swap targets more responsively. (Would sometimes shoot 1 extra shot at previous target even if they were out of range).
    To do:
    Add new abilities for second champion
    Research pathing AI for minions
    Continue to research Unity multiplayer capability

--- MOBA Champion Template/Assets/Scripts/Turret_Detection.cs
+++ MOBA Champion Template/Assets/Scripts/Turret_Detection.cs
@@ -3,48 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret_Detection : MonoBehaviour {
-	public bool red;
-	public Turret_AI t;
+	public bool red; //true = "this is a red turret". false = "this is a blue turret"
+	public Turret_AI t;	//Turret_AI script that is attached to the turret this script is attached to
 
+	//list of any targets that are in range
 	public List<GameObject> targets;
 
-	//public GameObject tar;
-
 	void OnTriggerEnter(Collider col)	{
-		if (red == true) { // red bullet
+		//if this turret is red and a blue champion or minion enters range, add it to the list of possible targets
+		if (red == true) { 
 			if (col.gameObject.tag == "Blue_Champ" || col.gameObject.tag == "Blue_Minion") {
 				Debug.Log (col.gameObject.name + " ADDED TO TARGET LIST");
 				targets.Add (col.gameObject);
 				t.setTarget (targets[0]);
-				//tar = col.gameObject;
 			}
 		}
-		else //red == false (blue bullet)
+		else //red == false (blue turret)
 		{
 			if (col.gameObject.tag == "Red_Champ" || col.gameObject.tag == "Red_Minion") {
 				Debug.Log (col.gameObject.name + " ADDED TO TARGET LIST");
 				targets.Add (col.gameObject);
 				t.setTarget (targets[0]);
-				//tar = col.gameObject;
 			}
 		}
 	}
 
 	void OnTriggerExit(Collider col)	{
-		if (red == true) { // red bullet
+		//if this turret is red and the object that left range is a blue champ or minion, remove it from the list of possible targets
+		if (red == true) {
 			if (col.gameObject.tag == "Blue_Champ" || col.gameObject.tag == "Blue_Minion") {
 				Debug.Log (col.gameObject.name + " REMOVED FROM TARGET LIST");
 				targets.Remove (col.gameObject);
 			}
 		} 
+		//if this turret is blue and the object that left range is a red champ or minion, remove it from the list of possible targets
 		else {
 			if (col.gameObject.tag == "Red_Champ" || col.gameObject.tag == "Red_Minion") {
 				Debug.Log (col.gameObject.name + " REMOVED FROM TARGET LIST");
 				targets.Remove (col.gameObject);
 			}	
 		}
+
+		//if there are no more targets in range, stop firing
 		if (targets.Count == 0)
 			t.cancelFire ();
+		//otherwise, acquire new target
+		else
+			t.setTarget (targets[0]);
 	}
 
 	// Use this for initialization
